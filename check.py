80bd1d2d26298e723de270f5432b33e19dd2e7ec
LeeYi-user <khes60122@gmail.com>
2024-10-21 01:16:05 +0800

    add gpu

--- check.py
+++ check.py
@@ -1,7 +1,9 @@
 import onnxruntime as ort
+from tensorflow.python.client import device_lib
 
 # 查看可用的執行提供者（Execution Providers）
 providers = ort.get_available_providers()
 print(providers)
+print(device_lib.list_local_devices())
 
 # 如果有 GPU，應該能看到 CUDA 的提供者
